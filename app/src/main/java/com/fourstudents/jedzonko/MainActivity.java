18bc0f64fe64bead1267f60895c62eb135d5907b
Kamil Krzyśków <kamilzary@gmail.com>
2021-03-26 03:11:29 +0100

    removed old comments

--- app/src/main/java/com/fourstudents/jedzonko/MainActivity.java
+++ app/src/main/java/com/fourstudents/jedzonko/MainActivity.java
@@ -21,36 +21,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        ArrayList<Todo> todos = new ArrayList<>();
-//        todos.add(new Todo("Item 1", true));
-//        todos.add(new Todo("Item 2", false));
-//        todos.add(new Todo("Item 3", false));
-//        todos.add(new Todo("Item 4", true));
-//        todos.add(new Todo("Item 5", false));
-//        todos.add(new Todo("Item 6", true));
-//
-//        TodoAdapter adapter = new TodoAdapter(todos);
-//        RecyclerView rvTodo = findViewById(R.id.rvTodo);
-//        rvTodo.setAdapter(adapter);
-//        rvTodo.setLayoutManager(new LinearLayoutManager(this));
-//
-//        (findViewById(R.id.btnAddTodo)).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String title = ((EditText) findViewById(R.id.etTodo)).getText().toString();
-//                Todo todo = new Todo(title, false);
-//                todos.add(todo);
-//                adapter.notifyItemInserted(todos.size() - 1);
-//            }
-//        });
-
         Fragment searchFragment = new SearchFragment();
         Fragment shoppingListFragment = new ShoppingListFragment();
         Fragment recipesFragment = new RecipesFragment();
         Fragment shopsFragment = new ShopsFragment();
         Fragment accountFragment = new AccountFragment();
-
-//        setCurrentFragment(recipesFragment);
 
         ((BottomNavigationView) findViewById(R.id.bottomNavigationView)).setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {

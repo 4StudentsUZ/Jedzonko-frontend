1e506f13a2fc81749cca9b9498f8bf372e1a8c95
Marcin Karwat <markar24@gmail.com>
2021-05-18 18:01:40 +0200

    Add "search for restaurants/groceries"

--- app/src/main/java/com/fourstudents/jedzonko/Fragments/Shop/ShopsFragment.java
+++ app/src/main/java/com/fourstudents/jedzonko/Fragments/Shop/ShopsFragment.java
@@ -1,5 +1,7 @@
 package com.fourstudents.jedzonko.Fragments.Shop;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,8 +11,6 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
-import com.fourstudents.jedzonko.Fragments.Recipe.AddTagFragment;
-import com.fourstudents.jedzonko.Fragments.Recipe.EditRecipeFragment;
 import com.fourstudents.jedzonko.R;
 
 public class ShopsFragment extends Fragment {
@@ -24,5 +24,25 @@ public class ShopsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Toolbar toolbar = view.findViewById(R.id.custom_toolbar);
         toolbar.setTitle(R.string.title_shops);
+
+        Button restaurantsButton = view.findViewById(R.id.restaurantsButton);
+        Button groceriesButton = view.findViewById(R.id.groceriesButton);
+
+        restaurantsButton.setOnClickListener(v -> openNearbyRestaurantsMap());
+        groceriesButton.setOnClickListener(v -> openNearbyGroceries());
+    }
+
+    private void openNearbyRestaurantsMap() {
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=restaurants");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+
+    private void openNearbyGroceries() {
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=grocery stores");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }

a9991af05124cb76c85efb7425197c8e1e2a0472
Marcin Karwat <markar24@gmail.com>
2021-05-18 21:38:07 +0200

    Use localization API when looking for groceries/restaurants

--- app/src/main/java/com/fourstudents/jedzonko/Fragments/Shop/ShopsFragment.java
+++ app/src/main/java/com/fourstudents/jedzonko/Fragments/Shop/ShopsFragment.java
@@ -1,22 +1,41 @@
 package com.fourstudents.jedzonko.Fragments.Shop;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.fourstudents.jedzonko.R;
 
+import org.jetbrains.annotations.NotNull;
+
 public class ShopsFragment extends Fragment {
+
+    private static final int locationRequestId = 24572;
+    private LocationManager locationManager;
 
     public ShopsFragment() {
         super(R.layout.fragment_shop);
+    }
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initLocationWithPermissionRequest();
     }
 
     @Override
@@ -28,21 +47,70 @@ public class ShopsFragment extends Fragment {
         Button restaurantsButton = view.findViewById(R.id.restaurantsButton);
         Button groceriesButton = view.findViewById(R.id.groceriesButton);
 
-        restaurantsButton.setOnClickListener(v -> openNearbyRestaurantsMap());
-        groceriesButton.setOnClickListener(v -> openNearbyGroceries());
+        restaurantsButton.setOnClickListener(v -> queryMaps("restaurants"));
+        groceriesButton.setOnClickListener(v -> queryMaps("grocery stores"));
     }
 
-    private void openNearbyRestaurantsMap() {
-        Uri gmmIntentUri = Uri.parse("geo:0,0?q=restaurants");
+    private void queryMaps(String query) {
+        Location location = getLastLocation();
+        if (location == null) {
+            Toast.makeText(requireActivity(), "Trwa ładowanie lokalizacji...", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Double latitude = location.getLatitude();
+        Double longitude = location.getLongitude();
+
+        @SuppressLint("DefaultLocale")
+        Uri gmmIntentUri = Uri.parse(String.format("geo:%f,%f?q=%s", latitude, longitude, query));
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         startActivity(mapIntent);
     }
 
-    private void openNearbyGroceries() {
-        Uri gmmIntentUri = Uri.parse("geo:0,0?q=grocery stores");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        startActivity(mapIntent);
+    private void initLocationWithPermissionRequest() {
+        if (locationManager != null) return;
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, locationRequestId);
+        } else {
+            initLocation();
+        }
+    }
+
+    private void initLocation() {
+        if (locationManager != null) return;
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                1000,
+                25,
+                location -> {}
+        );
+    }
+
+    private Location getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+        initLocationWithPermissionRequest();
+        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        if (requestCode == locationRequestId) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initLocation();
+            } else {
+                Toast.makeText(requireActivity(), "Brak pozwolenia na użycie lokalizacji.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }

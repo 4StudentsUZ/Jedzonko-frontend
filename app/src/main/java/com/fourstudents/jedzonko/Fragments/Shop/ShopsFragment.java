6690f58e5f75355db067142e05b98bf81d8601cf
Marcin Karwat <markar24@gmail.com>
2021-05-19 08:41:12 +0200

    Change the way location permissions are handled

--- app/src/main/java/com/fourstudents/jedzonko/Fragments/Shop/ShopsFragment.java
+++ app/src/main/java/com/fourstudents/jedzonko/Fragments/Shop/ShopsFragment.java
@@ -3,18 +3,22 @@ package com.fourstudents.jedzonko.Fragments.Shop;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
@@ -23,10 +27,16 @@ import com.fourstudents.jedzonko.R;
 
 import org.jetbrains.annotations.NotNull;
 
-public class ShopsFragment extends Fragment {
+public class ShopsFragment extends Fragment implements LocationListener {
 
     private static final int locationRequestId = 24572;
+    private Location lastLocation = null;
     private LocationManager locationManager;
+
+    private TextView statusText;
+    private Button requestPermissionsButton;
+    Button restaurantsButton;
+    Button groceriesButton;
 
     public ShopsFragment() {
         super(R.layout.fragment_shop);
@@ -35,7 +45,16 @@ public class ShopsFragment extends Fragment {
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initLocationWithPermissionRequest();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+            locationManager = null;
+        }
+
+        super.onDestroy();
     }
 
     @Override
@@ -44,22 +63,32 @@ public class ShopsFragment extends Fragment {
         Toolbar toolbar = view.findViewById(R.id.custom_toolbar);
         toolbar.setTitle(R.string.title_shops);
 
-        Button restaurantsButton = view.findViewById(R.id.restaurantsButton);
-        Button groceriesButton = view.findViewById(R.id.groceriesButton);
+        restaurantsButton = view.findViewById(R.id.restaurantsButton);
+        groceriesButton = view.findViewById(R.id.groceriesButton);
 
+        restaurantsButton.setEnabled(false);
         restaurantsButton.setOnClickListener(v -> queryMaps("restaurants"));
+
+        groceriesButton.setEnabled(false);
         groceriesButton.setOnClickListener(v -> queryMaps("grocery stores"));
+
+        statusText = view.findViewById(R.id.locationStatus);
+
+        requestPermissionsButton = view.findViewById(R.id.requestPermissions);
+        requestPermissionsButton.setVisibility(View.INVISIBLE);
+        requestPermissionsButton.setOnClickListener(v -> initLocationWithPermissionRequest());
+
+        initLocationWithPermissionRequest();
     }
 
     private void queryMaps(String query) {
-        Location location = getLastLocation();
-        if (location == null) {
+        if (lastLocation == null) {
             Toast.makeText(requireActivity(), "Trwa ładowanie lokalizacji...", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        Double latitude = location.getLatitude();
-        Double longitude = location.getLongitude();
+        Double latitude = lastLocation.getLatitude();
+        Double longitude = lastLocation.getLongitude();
 
         @SuppressLint("DefaultLocale")
         Uri gmmIntentUri = Uri.parse(String.format("geo:%f,%f?q=%s", latitude, longitude, query));
@@ -72,7 +101,17 @@ public class ShopsFragment extends Fragment {
         if (locationManager != null) return;
 
         if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, locationRequestId);
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, locationRequestId);
+            } else {
+                new AlertDialog.Builder(requireContext())
+                        .setTitle("Brak uprawnień")
+                        .setMessage("Brak pozwolenia na użycie lokalizacji, upewnij się, że aplikacja ma nadane odpowiednie uprawnienia w ustawieniach systemowych.")
+                        .setPositiveButton(android.R.string.ok, (dialog, which) -> {})
+                        .show();
+
+                onLocationPermissionDenied();
+            }
         } else {
             initLocation();
         }
@@ -87,30 +126,54 @@ public class ShopsFragment extends Fragment {
         locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(
                 LocationManager.GPS_PROVIDER,
-                1000,
-                25,
-                location -> {}
+                3000,
+                0,
+                this
         );
-    }
 
-    private Location getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return null;
-        }
-        initLocationWithPermissionRequest();
-        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        statusText.setText("Status:\nTrwa wyszukiwanie lokalizacji...\nJeśli to trwa zbyt długo, upewnij się, że włączyłeś usługę lokalizacji w telefonie.");
+        requestPermissionsButton.setVisibility(View.INVISIBLE);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
         if (requestCode == locationRequestId) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                initLocation();
+            if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                onLocationPermissionDenied();
             } else {
-                Toast.makeText(requireActivity(), "Brak pozwolenia na użycie lokalizacji.", Toast.LENGTH_SHORT).show();
+                requestPermissionsButton.setVisibility(View.VISIBLE);
+                initLocation();
             }
         }
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        lastLocation = location;
+        restaurantsButton.setEnabled(true);
+        groceriesButton.setEnabled(true);
+        statusText.setText("Status:\nOdnaleziono lokalizację!");
+    }
+
+    private void onLocationPermissionDenied() {
+        statusText.setText("Status:\nBrak pozwolenia na użycie lokalizacji...");
+        requestPermissionsButton.setVisibility(View.VISIBLE);
+
+        restaurantsButton.setEnabled(false);
+        groceriesButton.setEnabled(false);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
     }
 }
